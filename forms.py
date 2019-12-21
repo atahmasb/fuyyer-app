6238566d76043f9193e422e1c346d3f12ef59399
Amir <amir.tahmasbi22@gmail.com>
2019-12-20 17:55:12 -0800

    finish all the endpoints

--- forms.py
+++ forms.py
@@ -13,10 +13,11 @@ class VenueFields(Enum):
     ADDRESS = 'address'
     PHONE  = 'phone'
     IMAGE_LINK = 'image_link'
-    GENRES = 'genre'
+    GENRES = 'genres'
     FACEBOOK_LINK = 'facebook_link'
     SEEKING_TALENT = 'seeking new talent'
     SEEKING_DESCRIPTION = 'seeking description'
+    WEBSITE = 'website'
 
 
 class ArtistFields(Enum):
@@ -26,11 +27,12 @@ class ArtistFields(Enum):
     ADDRESS = 'address'
     PHONE  = 'phone'
     IMAGE_LINK = 'image_link'
-    GENRES = 'genre'
+    GENRES = 'genres'
     FACEBOOK_LINK = 'facebook_link'
     SEEKING_TALENT = 'seeking new talent'
     SEEKING_DESCRIPTION = 'seeking description'
     SEEKING_VENUE = 'seeking venue'
+    WEBSITE = 'website'
 
 
 def validate_phone(form, phone):
@@ -158,6 +160,117 @@ class VenueForm(Form):
         VenueFields.SEEKING_DESCRIPTION, validators=[validators.Length(min=10, max=500)])
     image_link = StringField(
         VenueFields.IMAGE_LINK, validators=[DataRequired, validators.Length(min=10, max=500)])
+    website = StringField(
+        VenueFields.WEBSITE, validators=[DataRequired, validators.Length( max=120)])
+
+
+class EditVenueForm(Form):
+    name = StringField(
+        VenueFields.NAME
+    )
+    city = StringField(
+        VenueFields.CITY
+    )
+    state = SelectField(
+        VenueFields.STATE,
+        choices=[
+            ('AL', 'AL'),
+            ('AK', 'AK'),
+            ('AZ', 'AZ'),
+            ('AR', 'AR'),
+            ('CA', 'CA'),
+            ('CO', 'CO'),
+            ('CT', 'CT'),
+            ('DE', 'DE'),
+            ('DC', 'DC'),
+            ('FL', 'FL'),
+            ('GA', 'GA'),
+            ('HI', 'HI'),
+            ('ID', 'ID'),
+            ('IL', 'IL'),
+            ('IN', 'IN'),
+            ('IA', 'IA'),
+            ('KS', 'KS'),
+            ('KY', 'KY'),
+            ('LA', 'LA'),
+            ('ME', 'ME'),
+            ('MT', 'MT'),
+            ('NE', 'NE'),
+            ('NV', 'NV'),
+            ('NH', 'NH'),
+            ('NJ', 'NJ'),
+            ('NM', 'NM'),
+            ('NY', 'NY'),
+            ('NC', 'NC'),
+            ('ND', 'ND'),
+            ('OH', 'OH'),
+            ('OK', 'OK'),
+            ('OR', 'OR'),
+            ('MD', 'MD'),
+            ('MA', 'MA'),
+            ('MI', 'MI'),
+            ('MN', 'MN'),
+            ('MS', 'MS'),
+            ('MO', 'MO'),
+            ('PA', 'PA'),
+            ('RI', 'RI'),
+            ('SC', 'SC'),
+            ('SD', 'SD'),
+            ('TN', 'TN'),
+            ('TX', 'TX'),
+            ('UT', 'UT'),
+            ('VT', 'VT'),
+            ('VA', 'VA'),
+            ('WA', 'WA'),
+            ('WV', 'WV'),
+            ('WI', 'WI'),
+            ('WY', 'WY'),
+        ]
+    )
+    address = StringField(
+        VenueFields.ADDRESS
+    )
+    phone = StringField(
+        VenueFields.PHONE, validators=[validate_phone]
+    )
+
+    genres = SelectMultipleField(
+        VenueFields.GENRES,
+        validators=[validators.Optional()],
+        choices=[
+            ('Alternative', 'Alternative'),
+            ('Blues', 'Blues'),
+            ('Classical', 'Classical'),
+            ('Country', 'Country'),
+            ('Electronic', 'Electronic'),
+            ('Folk', 'Folk'),
+            ('Funk', 'Funk'),
+            ('Hip-Hop', 'Hip-Hop'),
+            ('Heavy Metal', 'Heavy Metal'),
+            ('Instrumental', 'Instrumental'),
+            ('Jazz', 'Jazz'),
+            ('Musical Theatre', 'Musical Theatre'),
+            ('Pop', 'Pop'),
+            ('Punk', 'Punk'),
+            ('R&B', 'R&B'),
+            ('Reggae', 'Reggae'),
+            ('Rock n Roll', 'Rock n Roll'),
+            ('Soul', 'Soul'),
+            ('Other', 'Other'),
+        ]
+    )
+    facebook_link = StringField(
+        VenueFields.FACEBOOK_LINK, validators=[URL()]
+    )
+    seeking_talent = BooleanField(
+        VenueFields.SEEKING_TALENT )
+
+    seeking_description = StringField(
+        VenueFields.SEEKING_DESCRIPTION, validators=[validators.Length(min=10, max=500)])
+    image_link = StringField(
+        VenueFields.IMAGE_LINK, validators=[validators.Length(min=10, max=500)])
+    website = StringField(
+        VenueFields.WEBSITE, validators=[validators.Length( max=120)])
 
 
 class ArtistForm(Form):
@@ -167,6 +280,8 @@ class ArtistForm(Form):
     city = StringField(
         ArtistFields.CITY, validators=[DataRequired()]
     )
+    website = StringField(
+        ArtistFields.WEBSITE, validators=[DataRequired, validators.Length(max=120)])
     state = SelectField(
         ArtistFields.STATE, validators=[DataRequired()],
         choices=[
@@ -262,13 +377,108 @@ class ArtistForm(Form):
     seeking_description = StringField(
         ArtistFields.SEEKING_DESCRIPTION, validators=[validators.Length(min=10, max=500)])
 
+class EditArtistForm(Form):
+    name = StringField(
+       ArtistFields.NAME
+    )
+    city = StringField(
+        ArtistFields.CITY
+    )
+    website = StringField(
+        ArtistFields.WEBSITE, validators=[validators.Length(max=120)])
+    state = SelectField(
+        ArtistFields.STATE,
+        choices=[
+            ('AL', 'AL'),
+            ('AK', 'AK'),
+            ('AZ', 'AZ'),
+            ('AR', 'AR'),
+            ('CA', 'CA'),
+            ('CO', 'CO'),
+            ('CT', 'CT'),
+            ('DE', 'DE'),
+            ('DC', 'DC'),
+            ('FL', 'FL'),
+            ('GA', 'GA'),
+            ('HI', 'HI'),
+            ('ID', 'ID'),
+            ('IL', 'IL'),
+            ('IN', 'IN'),
+            ('IA', 'IA'),
+            ('KS', 'KS'),
+            ('KY', 'KY'),
+            ('LA', 'LA'),
+            ('ME', 'ME'),
+            ('MT', 'MT'),
+            ('NE', 'NE'),
+            ('NV', 'NV'),
+            ('NH', 'NH'),
+            ('NJ', 'NJ'),
+            ('NM', 'NM'),
+            ('NY', 'NY'),
+            ('NC', 'NC'),
+            ('ND', 'ND'),
+            ('OH', 'OH'),
+            ('OK', 'OK'),
+            ('OR', 'OR'),
+            ('MD', 'MD'),
+            ('MA', 'MA'),
+            ('MI', 'MI'),
+            ('MN', 'MN'),
+            ('MS', 'MS'),
+            ('MO', 'MO'),
+            ('PA', 'PA'),
+            ('RI', 'RI'),
+            ('SC', 'SC'),
+            ('SD', 'SD'),
+            ('TN', 'TN'),
+            ('TX', 'TX'),
+            ('UT', 'UT'),
+            ('VT', 'VT'),
+            ('VA', 'VA'),
+            ('WA', 'WA'),
+            ('WV', 'WV'),
+            ('WI', 'WI'),
+            ('WY', 'WY'),
+        ]
+    )
+    phone = StringField(
+        ArtistFields.PHONE, validators=[validate_phone]
+    )
 
-    # def validate_phone(self, phone):
-    #     try:
-    #         p = phonenumbers.parse(phone.data)
-    #         if not phonenumbers.is_valid_number(p):
-    #             raise ValueError()
-    #     except (phonenumbers.phonenumberutil.NumberParseException, ValueError):
-    #         raise ValidationError('Invalid phone number')
+    genres = SelectMultipleField(
+
+        ArtistFields.GENRES,
+        choices=[
+            ('Alternative', 'Alternative'),
+            ('Blues', 'Blues'),
+            ('Classical', 'Classical'),
+            ('Country', 'Country'),
+            ('Electronic', 'Electronic'),
+            ('Folk', 'Folk'),
+            ('Funk', 'Funk'),
+            ('Hip-Hop', 'Hip-Hop'),
+            ('Heavy Metal', 'Heavy Metal'),
+            ('Instrumental', 'Instrumental'),
+            ('Jazz', 'Jazz'),
+            ('Musical Theatre', 'Musical Theatre'),
+            ('Pop', 'Pop'),
+            ('Punk', 'Punk'),
+            ('R&B', 'R&B'),
+            ('Reggae', 'Reggae'),
+            ('Rock n Roll', 'Rock n Roll'),
+            ('Soul', 'Soul'),
+            ('Other', 'Other'),
+        ]
+    )
+    facebook_link = StringField(
+        ArtistFields.FACEBOOK_LINK, validators=[URL()]
+    )
+    seeking_venue = BooleanField(
+        ArtistFields.SEEKING_VENUE)
+
+    seeking_description = StringField(
+        ArtistFields.SEEKING_DESCRIPTION, validators=[validators.Length(min=10, max=500)])
+
 
 # TODO IMPLEMENT NEW ARTIST FORM AND NEW SHOW FORM
